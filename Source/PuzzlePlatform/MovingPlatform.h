3cb9484e8fdef917f68a36d08aea7c429e96be6e
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-12-01 01:53:21 +0300

    Server authority function used for detecting which game is server which game is client.

---
+++ Source/PuzzlePlatform/MovingPlatform.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORM_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	AMovingPlatform();
+protected:
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere, Category = "Platform Settings")
+	float PlatformMovingSpeed = 5;
+
+};
