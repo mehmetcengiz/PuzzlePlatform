d957049685c9ca07a384348dba7cd68e7159c066
Mehmet Cengiz <m.cengiz4716@gmail.com>
2018-03-15 23:59:11 +0300

    Players travel to game map when all 3 is in Lobby. Seamless travel enabled.

--- Source/PuzzlePlatform/LobbyGameMode.cpp
+++ Source/PuzzlePlatform/LobbyGameMode.cpp
@@ -7,7 +7,13 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer) {
 	Super::PostLogin(NewPlayer);
 	++NumberOfPlayers;
 	if(NumberOfPlayers >=3 ) {
-		UE_LOG(LogTemp, Warning, TEXT("Reached 3 players!"));
+		UE_LOG(LogTemp, Warning, TEXT("Reached 3 players!"));	
+		
+		UWorld* World = GetWorld();
+		if (!ensure(World != nullptr)) return;
+
+		bUseSeamlessTravel = true;
+		World->ServerTravel("/Game/PuzzlePlatforms/Maps/Game?listen");
 	}
 }
 
