fe0eb0746318ab95a9d0208f9dab54a00b587d13
mehmetcengiz <mehmetceng7@gmail.com>
2018-03-21 13:45:11 +0300

    Clean up.

--- Source/PuzzlePlatform/PuzzlePlatformGameMode.cpp
+++ Source/PuzzlePlatform/PuzzlePlatformGameMode.cpp
@@ -7,7 +7,7 @@
 APuzzlePlatformGameMode::APuzzlePlatformGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Character/Blueprints/BP_VRPawn"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
